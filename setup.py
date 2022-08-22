56ea3fdd83d4d845ac8e993ce962b628b5121380
Sonja Offwood-de Klerk <sonja@growthintelligence.com>
2022-08-22 10:16:18 +0100

    scikit at 0.24 for 3.9 due to breaking changes

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ py36_requirements = [
 ]
 
 py39_requirements = [
-       'scikit-learn==1.1.1',
+       'scikit-learn==0.24.2',
        'numpy==1.23.0', # require a version which is compatible with py3.9 and is > 1.20 due to API changes  
        'pandas~=1.4.3',
        'scipy~=1.9.0',
