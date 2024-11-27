e648fed247fcad750961f3687123c747efb444fc
alexanderjmitchell <alexanderjmitchell@gmail.com>
2024-11-27 16:53:07 +0000

    Numpy nump

--- setup.py
+++ setup.py
@@ -4,30 +4,14 @@ import sys
 requirements = [
           'matplotlib>=3.0.0',
           'joblib>=1.0.0',
+          'scikit-learn==1.1.2',
+          'numpy<=1.23.5', # require a version which is compatible with py3.9 and is > 1.20 due to API changes  
+          'pandas~=1.5',
+          'scipy~=1.10.0',
 ]
-
-py36_requirements = [
-           'scikit-learn>=0.20.3',
-           'numpy<1.20', # last version supported by python 3.6  
-           'pandas~=0.24.0',
-           'scipy~=1.1.0',
-]
-
-py39_requirements = [
-       'scikit-learn==1.1.2',
-       'numpy==1.23.0', # require a version which is compatible with py3.9 and is > 1.20 due to API changes  
-       'pandas~=1.5',
-       'scipy~=1.10.0',
-]
-
-if sys.version_info[0] == 3 and sys.version_info[1] == 6:
-    requirements += py36_requirements
-else:
-    requirements += py39_requirements
-
 
 setup(name='mlxtend',
-      version='0.0.4',
+      version='0.1.0',
       description='Machine Learning Library Extensions',
       url='https://github.com/pelucid/mlxtend',
       packages=find_packages(),
